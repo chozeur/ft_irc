024dc52c4a63942915ab90c74b13d7ec147190b2
Florian CARVALHO <flcarval@student.42.fr>
2023-02-26 16:59:38 +0100

    added comments on server.hpp

--- srcs/server/server.hpp
+++ srcs/server/server.hpp
@@ -3,35 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   server.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
+/*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 15:15:36 by tbrebion          #+#    #+#             */
-/*   Updated: 2023/02/25 19:52:25 by tbrebion         ###   ########.fr       */
+/*   Updated: 2023/02/26 16:57:55 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include "../config/config.hpp"
 
 
-namespace irc{
+namespace irc{ //? name the namesapce ft_irc
 
-	class server{
+	class server{	//? respect coplien form
 
 		public:
 			server();
 			~server();
 
-			irc::config &getConf();
+			irc::config &getConf();	//? set to const and make a typedef for irc::config
 			struct sockaddr_in	*getServAddr()const;
 			int	getSockfd()const;
 			void	setSockfd(int fd);
-			
+
 		private:
 			irc::config conf;
 			struct sockaddr_in *serv_addr;
