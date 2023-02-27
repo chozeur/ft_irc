dcae63671d5678553a876d1cd5788788b43eb7f5
tbrebion <titouan.brebion@yahoo.com>
2023-02-27 13:04:26 +0100

    exception OK

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 21:16:58 by tbrebion          #+#    #+#             */
-/*   Updated: 2023/02/27 12:07:55 by tbrebion         ###   ########.fr       */
+/*   Updated: 2023/02/27 13:03:58 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,17 @@ int	main(int ac, char **av, char **env){
 
 	ft_irc::server	serv = ft_irc::server();
 	long			port;
-	std::string		password = av[2];
+	std::string		password = av[2] ? av[2] : "no password";
 
 	try {
 		port = parsing_args(ac, av, env);
 		serv.init(password, port);
 	} catch (std::exception &e){
+		std::cerr << e.what() << std::endl;
 		return (1);
 	}
 
 	signal(SIGINT, sig_handler);
-	std::cout << serv.getPort() << std::endl << serv.getPassword() << std::endl;
 	while (!stop){
 
 		//server.run(); //? server.run() should be called only once..?
--- srcs/utils/parsing_args.cpp
+++ srcs/utils/parsing_args.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing_args.cpp                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 23:17:42 by flcarval          #+#    #+#             */
-/*   Updated: 2023/02/27 00:45:37 by flcarval         ###   ########.fr       */
+/*   Updated: 2023/02/27 13:01:55 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ long	parsing_args(int ac, char **av, char **env){
 		throw std::runtime_error("Error : no environment variables");
 	if (ac != 3)
 		throw std::runtime_error("Error : ./ircserv <port> <password>");
+	if (av[2][0] == '\0')
+		throw std::runtime_error("Error : password's size must at least 1");
 	if (av[1][0] == '\0')
 		throw std::runtime_error("Error : port must be a number between 1 and 65535");
 	port = strtol(av[1], &endptr, port);
