6811dba4cd6ffa08096d207ec7d4d18d51fb4ea8
Florian CARVALHO <flcarval@student.42.fr>
2023-03-15 12:24:31 +0100

    wiped Chozeur

--- srcs/utils/removeOccurences.cpp
+++
@@ -1,8 +0,0 @@
-#include "../../includes/utils.hpp"
-
-void removeAllOccurrences(std::string& str, const std::string& substr) {
-    size_t pos = 0;
-    while ((pos = str.find(substr, pos)) != std::string::npos) {
-        str.erase(pos, substr.length());
-    }
-}
