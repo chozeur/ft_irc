9c8a0bbacd2dc4403842b0e844de1d361f1ac4c4
tbrebion <titouan.brebion@yahoo.com>
2023-04-01 20:52:53 +0200

    /whois in progress

--- srcs/server/server.cpp
+++ srcs/server/server.cpp
@@ -203,7 +203,7 @@ void 	ft_irc::Server::initChannels() {
 
 void	ft_irc::Server::run(void) {
 	while (server) {
-		std::string pong = "PONG\n";
+		std::string pong = "PONG\r\n";
 		sendToAllClients(pong);
 		int num_ready_fds = poll(this->_fds, MAX_CLIENTS + 1, -1);
 		if (num_ready_fds == -1) {
